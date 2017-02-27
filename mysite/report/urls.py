1a4148942861094860ec463fc51948a8ad95672b
Shuang0420 <sxu1@andrew.cmu.edu>
2017-02-27 01:56:20 -0500

    form feedback

--- mysite/report/urls.py
+++ mysite/report/urls.py
@@ -6,6 +6,7 @@ app_name = 'mysample'
 urlpatterns = [
     # ex: /mysample/
     url(r'^$', views.index, name='index'),
+    url(r'^get_report/+$', views.get_report_by_title),
     # ex: /mysample/1/
     # url(r'^(?P<id>[0-9]+)/$', views.dashboard, name='dashboard'),
 ]
--- mysite/report/views.py
+++ mysite/report/views.py
@@ -2,8 +2,11 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.shortcuts import render
+from django.shortcuts import render_to_response
 from django.http import HttpResponse
 from django.template import loader
+from django.template.context_processors import csrf
+from django.template import RequestContext
 
 # Create your views here.
 '''
@@ -13,5 +16,28 @@ https://docs.djangoproject.com/en/1.10/intro/tutorial01/
 # Create your views here.
 def index(request):
     template = loader.get_template('report/main.html')
-    return HttpResponse(template.render(request))
-    # return HttpResponse("Hello, world. Go to /mysample/1 to see dashboard 1")
+    # return HttpResponse(template.render(request))
+    return render(request,'report/main.html')
+
+
+'''
+def get_report(request):
+    if 'program_title' in request.GET:
+        mes=request.GET['program_title']
+    return HttpResponse(mes)
+    '''
+
+def get_report_by_title(request):
+    ctx ={}
+    ctx.update(csrf(request))
+    if request.POST:
+    	ctx['program_title'] = request.POST['program_title']
+    	ctx['region'] = request.POST['region'] +','
+    	ctx['genre'] = request.POST['genre']
+    	ctx['date_range'] = 'Date Range: '
+    	ctx['date_from'] = request.POST['date_from']
+    	ctx['date_to'] = '- '+request.POST['date_to']
+    	ctx['time_range'] = 'Time Range: '
+    	ctx['time_from'] = request.POST['time_from']
+    	ctx['time_to'] = '- '+request.POST['time_to']
+    return render(request, 'report/main.html', ctx)
