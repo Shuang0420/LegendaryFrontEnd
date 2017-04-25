5fed884f434e0852ea61aae5017fdf51fc0c73c3
Shuang0420 <sxu1@andrew.cmu.edu>
2017-04-25 19:32:34 -0400

    add url pattern

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -22,20 +22,14 @@ from django.views.generic import RedirectView
 
 urlpatterns = [
     # point the root URLconf at the mysample.urls module
-    #url(r'^$', include('users.urls')),
-    #url(r'^users/', include('users.urls')),
-    #url(r'^users/$', include('users.urls')),
     url(r'^users/$',RedirectView.as_view(url='/admin/')),
     url(r'^admin/', include(admin.site.urls)),
-    #url(r'', include('users.urls')),
     url(r'^login/', views.login, {'template_name': 'users/login.html', 'authentication_form': LoginForm}, name='login'),
     url(r'^logout/$', views.logout, {'next_page': 'login'}, name='logout'),
-
     url(r'^$', include('dashboard.urls')),
-    #url(r'^$', include('dashboard.urls')),
     url(r'^dashboard/', include('dashboard.urls')),
     url(r'^report/', include('report.urls')),
-    url(r'^analytics/', RedirectView.as_view(url='/report/')),
+    url(r'^analytics/', include('analytics.urls')),
     url(r'^admin/', admin.site.urls),
     #url(r'^login/$', auth_views.login, name='login'),
     #url(r'^logout/$', auth_views.logout, name='logout'),
